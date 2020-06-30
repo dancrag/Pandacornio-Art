74c6bf42b1c74e54849cf4b2084d20797a50539a
dancrag <39243966+dancrag@users.noreply.github.com>
2020-06-29 19:42:14 -0500

    make it better

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router()
+
+//routes
+router.get('/index', (req, res) => {
+  res.render('index.html', {title: 'First website', subtitle: 'Pandacornio'});
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact.html', {title: 'Contact Page'});
+});
+
+router.get('/products', (req, res) => {
+res.render('products.html', {title: 'Products' })
+});
+
+module.exports = router;
