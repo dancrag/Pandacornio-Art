8441bf1063d30447d5000e8fa5bd6f5d6225909b
dancrag <39243966+dancrag@users.noreply.github.com>
2020-06-29 22:44:11 -0500

    Cambios en about

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,19 @@ const router = express.Router()
 
 //routes
 router.get('/', (req, res) => {
-  res.render('index.html', {title: 'First website', subtitle: 'Pandacornio'});
+  res.render('index.html', {title: 'Home', subtitle: 'Pandacornio.Art'});
 });
 
 router.get('/contact', (req, res) => {
-  res.render('contact.html', {title: 'Contact Page'});
+  res.render('contact.html', {title: 'Contact'});
 });
 
 router.get('/products', (req, res) => {
-res.render('products.html', {title: 'Products' })
+  res.render('products.html', {title: 'Products' })
+});
+
+router.get('/about', (req, res) => {
+  res.render('about.html', {title: 'About'})
 });
 
 module.exports = router;
