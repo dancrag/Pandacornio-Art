c5c9bbf617a5028a2cbb9c2bc30ea42e57003c75
dancrag <39243966+dancrag@users.noreply.github.com>
2020-07-05 16:52:16 -0500

    cambio en el string de conexión de bd

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://babadook:FikaX62664dunno$@pandacluster.yceml.mongodb.net/pandacluster?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://dancrag1:eFk7IKO3JaEGLMkw@pandacluster.yceml.mongodb.net/pandacluster?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
