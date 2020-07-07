27cf4f166916148884952114fdaf0fa343a78938
dancrag <39243966+dancrag@users.noreply.github.com>
2020-07-06 20:34:46 -0500

    se modifica el modelo Product

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 
 const ProductSchema = new Schema({
     name: {type: String, required: true},
+    url: {type: String, required: false},
     price: {type: Number, required: true},
     description: {type: String, required: true},
     date: {type: Date, default: Date.now}
