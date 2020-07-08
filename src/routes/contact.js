a4579636ef2fead29135860e0a97a78432a18124
dancrag <39243966+dancrag@users.noreply.github.com>
2020-07-07 21:49:02 -0500

    funcionalidad de correo lista

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -7,20 +7,24 @@ router.post('/send-email', async (req, res) => {
     const {name, email, phone, message} = req.body;
 
     const messageOps = {
-        from: 'danielcruz1im18@gmail.com',
         to: 'danielcruz1im18@gmail.com',
-        subject: ('Pedido Especial de', name, phone),
-        text: message 
+        cc: email,
+        subject: 'Pedido Especial de ' + name + ' ' + phone,
+        text: message
     };
 
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: '587',
-        secure: true,
+        pool: true,
+        service: 'Gmail',
+        //host: 'smtp.gmail.com',
+        //port: '465',
+        //secure: true,
         auth: {
             type: 'OAuth2',
             user: 'danielcruz1im18@gmail.com',
-            pass: 'mentehaurire28'
+            refreshToken: '1//04BeKBUp4bq5LCgYIARAAGAQSNwF-L9IrpjgowUCHwKjjOqm-Nu-Um7TcTXV6OO_yYlQ1Z4PftASusPDJ-0YSRZ7C3IbeQybvqGc',
+            clientId: '859027338619-i4ct9kjpina85l20qbrdbio71apnqc5t.apps.googleusercontent.com',
+            clientSecret: 'XdKTFw1b_xP5-7Db8HcBJfdw',
         }
     });
 
@@ -29,10 +33,14 @@ router.post('/send-email', async (req, res) => {
             console.log(err);
             
         } else {
-            console.log(info);
+            transporter.on('token', token => {
+                console.log('A new access token was generated');
+                console.log('User: %s', token.user);
+                console.log('Access Token: %s', token.accessToken);
+                console.log('Expires: %s', new Date(token.expires));
+            });
         }
     });
-
     req.flash('success_msg', '¡Se envió tu correo, pronto te contactaremos!');
     res.redirect('/');
 }); 
